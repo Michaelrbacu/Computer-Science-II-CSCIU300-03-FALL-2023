6e481f01fe7a28d2e385961f46aeda799ad58392
Michael Bacu <64717212+Michaelrbacu@users.noreply.github.com>
2023-09-21 12:23:57 -0400

    Add files via upload

---
+++ Assignments/4/Sum.java
@@ -0,0 +1,23 @@
+/*
+Sum.java
+Michael Bacu
+U300
+*/
+public class Sum{
+
+public static void main(String[] args){
+   if(args.length>=2){
+   
+   System.out.println("a");
+   System.out.println("b"+ args[0]);
+   System.out.println("c" + args[1]);
+   
+   //or(int index=0; index(args.length;index++)
+   //System.out.println(args[index]);
+   for(String str:args)
+      System.out.println(str);
+   
+   
+   }
+}
+}
