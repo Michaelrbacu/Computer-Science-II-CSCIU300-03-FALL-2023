7af16c2a9480813278ec71d5164a0472740dd972
michael <michaelrbacu2@gmail.com>
2023-10-12 15:54:51 -0400

    asd

--- Practice/Arrays/TwoDArrayExample.java
+++ Practice/Arrays/TwoDArrayExample.java
@@ -17,5 +17,21 @@ public class TwoDArrayExample {
             }
             System.out.println(); // Move to the next row
         }
+        System.out.println("");
+        System.out.println("");
+
+            // Declare a 2D array with 3 rows and 4 columns
+int[][] twoDArray = new int[3][4];
+
+int element = twoDArray[0][1];
+
+for (int i = 0; i < 3; i++) {
+    for (int j = 0; j < 4; j++) {
+        System.out.print(twoDArray[i][j] + " ");
+    }
+    System.out.println(); // Move to the next row
+}
+
+
     }
 }
