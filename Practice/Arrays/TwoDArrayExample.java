4ed9bdcf0acae8c8171acc097d3fcb2f41d5fe76
michael <michaelrbacu2@gmail.com>
2023-10-15 14:08:34 -0400

    practicing for quiz

--- Practice/Arrays/TwoDArrayExample.java
+++ Practice/Arrays/TwoDArrayExample.java
@@ -18,18 +18,5 @@ public class TwoDArrayExample {
         System.out.println("");
         System.out.println("");
 
-int[][] twoDArray = new int[3][4];
-
-int element = twoDArray[0][1];
-
-for (int i = 0; i < 3; i++) {
-    for (int j = 0; j < 4; j++) {
-        System.out.print(twoDArray[i][j] + " ");
-    }
-    System.out.println(); 
-}
-
-
-
     }
 }
