8413d4fb218a41c34244f74dd1d155e83ea52000
michael <michaelrbacu2@gmail.com>
2023-10-23 19:55:23 -0400

    asd

--- Assignments/4/Parameters.java
+++ Assignments/4/Parameters.java
@@ -6,10 +6,7 @@
 
 public class Parameters
 {
-    //-----------------------------------------------
-    // Calls the average method with
-    // different numbers of parameters.
-    //-----------------------------------------------
+
     public static void main(String[] args)
     {
         double mean1, mean2, min1, min2;
@@ -28,16 +25,7 @@ public class Parameters
         System.out.println(average());
         
     }
-    //----------------------------------------------
-    // Returns the average of its parameters.
-    //----------------------------------------------
-    
-    /*
-      1.	Compile and run the program. 
-      2.	Add a call to find the average of a single integer, say 13. Print the result of the call.
-      3.	Add a call with an empty parameter list and print the result. Is the behavior what you expected?
-      4.	Add a method minimum that takes a variable number of integer parameters and returns the minimum of the parameters. Invoke your method on each of the parameter lists used for the average function.
-    */
+
     
     public static double average (int ... list)
     {
@@ -74,19 +62,3 @@ public static double min (int ... args)
    return 0;
 }
 }
-
-/*public double average(int . . . list)
- * {
- *  double result =0.0;
- *  if (list.length!=0)
- *      {
- *      int sum=0;
- *      for (int num: list)
- *          {
- *          sum+=num;
- *          }
- *      result=(double)sum/list.length
- *      }
- *  return result;
- * }
- */
