40e4eee38aa6048ceb6d419030a66a5bc48fef38
michael <michaelrbacu2@gmail.com>
2023-10-27 00:26:47 -0400

    assignment 7 start

---
+++ Assignments/7/BusinessAccount.java
@@ -0,0 +1,15 @@
+
+public abstract class BusinessAccount extends BankAccount{
+    public final double monthlyfee = 20; //monthly fee
+    //constructor
+    public BusinessAccount(String name, String address, String email, String phone)
+    {
+        super(name, address, email, phone); //call parent's constructor
+    }
+    
+    @Override
+    public void addfee()
+    {
+        balance -= monthlyfee;
+    }
+}
