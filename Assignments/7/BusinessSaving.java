9ebcbe515aa07057881391f7b13e07d4689ebfd0
michael <michaelrbacu2@gmail.com>
2023-11-02 01:10:32 -0400

    asd

--- Assignments/7/BusinessSaving.java
+++ Assignments/7/BusinessSaving.java
@@ -1,16 +1,14 @@
 public abstract class BusinessSaving extends BusinessAccount{
-    public final double interestrate = .025;
     public double fee = 10;
 
     //constructor
-    public BusinessSaving(String name, String address, String email, String phone)
+    public BusinessSaving(String name, String address, String email, String phone, double interestrate)
     {
-        super(name, address, email, phone); //call parent's constructor
+        super(name, address, email, phone);
+        interestrate = .025;
+        //call parent's constructor
     }
     
-    public void interestRate()
-    {
-        balance *= interestrate;
-    }
+
 
 }
