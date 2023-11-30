34236b04ce12427ef9369e45829289816ad7916c
michael <michaelrbacu2@gmail.com>
2023-11-30 00:43:40 -0500

    practice

--- Practice/ExceptionExample.java
+++ Practice/ExceptionExample.java
@@ -1,6 +1,7 @@
 package Practice;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
@@ -8,34 +9,55 @@ public class ExceptionExample {
 
     public static void main(String[] args) {
         try {
-            // Example 1: ArithmeticException
+            // Code that may throw an exception
             int result = 10 / 0; // This will throw an ArithmeticException
-
-            // Example 2: ArrayIndexOutOfBoundsException
-            int[] arr = new int[5];
-            int value = arr[10]; // This will throw an ArrayIndexOutOfBoundsException
-
-            // Example 3: NullPointerException
-            String str = null;
-            int length = str.length(); // This will throw a NullPointerException
-
-            // Example 4: IOException
-            readFile("nonexistent_file.txt");
-
         } catch (ArithmeticException e) {
-            System.out.println("Caught ArithmeticException: " + e.getMessage());
-        } catch (ArrayIndexOutOfBoundsException e) {
-            System.out.println("Caught ArrayIndexOutOfBoundsException: " + e.getMessage());
-        } catch (NullPointerException e) {
-            System.out.println("Caught NullPointerException: " + e.getMessage());
-        } catch (IOException e) {
-            System.out.println("Caught IOException: " + e.getMessage());
-        } catch (Exception e) {
-            // This will catch any other exceptions not explicitly handled above
-            System.out.println("Caught an unexpected exception: " + e.getMessage());
-        } finally {
-            System.out.println("Finally block executed.");
+            // Handling the exception
+            System.out.println("Error: " + e.getMessage());
         }
+        try {
+            // Code that may throw an exception
+            String str = null;
+            System.out.println(str.length()); // This will throw a NullPointerException
+        } catch (ArithmeticException e) {
+            // Handling ArithmeticException
+            System.out.println("ArithmeticException: " + e.getMessage());
+        } catch (NullPointerException e) {
+            // Handling NullPointerException
+            System.out.println("NullPointerException: " + e.getMessage());
+        } catch (Exception e) {
+            // Handling other exceptions
+            System.out.println("Exception: " + e.getMessage());
+        }
+
+        try {
+            // Code that may throw an exception
+            FileReader file = new FileReader("example.txt");
+            // Perform file operations
+        } catch (FileNotFoundException e) {
+            // Handling FileNotFoundException
+            System.out.println("File not found: " + e.getMessage());
+        } finally {
+            // Code in the finally block will always be executed
+            System.out.println("Finally block: Closing resources");
+        }
+
+        try {
+            // Outer try block
+            try {
+                // Inner try block
+                int[] array = new int[5];
+                System.out.println(array[10]); // This will throw an ArrayIndexOutOfBoundsException
+            } catch (ArrayIndexOutOfBoundsException e) {
+                // Handling ArrayIndexOutOfBoundsException inside the inner catch block
+                System.out.println("Inner Catch: " + e.getMessage());
+            }
+            // Code after the inner try-catch block
+        } catch (Exception e) {
+            // Handling other exceptions in the outer catch block
+            System.out.println("Outer Catch: " + e.getMessage());
+        }
+
     }
 
     private static void readFile(String filename) throws IOException {
