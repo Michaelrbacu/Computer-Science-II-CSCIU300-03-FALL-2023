fa9a79137e5e64ac31d62571c81a9396be14725c
michael <michaelrbacu2@gmail.com>
2023-12-02 00:01:00 -0500

    asd

--- Practice/RecursiveExample.java
+++ Practice/RecursiveExample.java
@@ -1,14 +1,14 @@
 public class RecursiveExample {
 
     public static void main(String[] args) {
-        // Example 1: Basic recursive try-catch
+        
         try {
             recursiveMethod1(5);
         } catch (Exception e) {
             System.out.println("Example 1 - Exception caught: " + e.getMessage());
         }
 
-        // Example 2: Recursive with multiple catch blocks
+        
         try {
             recursiveMethod2(5);
         } catch (ArithmeticException e) {
@@ -17,7 +17,7 @@ public class RecursiveExample {
             System.out.println("Example 2 - RuntimeException caught: " + e.getMessage());
         }
 
-        // Example 3: Recursive with finally block
+        
         try {
             recursiveMethod3(5);
         } catch (Exception e) {
@@ -27,24 +27,24 @@ public class RecursiveExample {
         }
     }
 
-    // Example 1: Basic recursive try-catch
+    
     private static void recursiveMethod1(int n) {
         if (n > 0) {
-            // Recursive call
+            
             recursiveMethod1(n - 1);
-            // Code that may throw an exception
+            
             if (n == 3) {
                 throw new RuntimeException("Example 1 - Exception at n=3");
             }
         }
     }
 
-    // Example 2: Recursive with multiple catch blocks
+    
     private static void recursiveMethod2(int n) {
         if (n > 0) {
-            // Recursive call
+            
             recursiveMethod2(n - 1);
-            // Code that may throw exceptions
+            
             if (n % 2 == 0) {
                 throw new ArithmeticException("Example 2 - ArithmeticException at even n");
             }
@@ -54,13 +54,13 @@ public class RecursiveExample {
         }
     }
 
-    // Example 3: Recursive with finally block
+    
     private static void recursiveMethod3(int n) {
         if (n > 0) {
             try {
-                // Recursive call
+                
                 recursiveMethod3(n - 1);
-                // Code that may throw an exception
+                
                 if (n == 3) {
                     throw new RuntimeException("Example 3 - Exception at n=3");
                 }
