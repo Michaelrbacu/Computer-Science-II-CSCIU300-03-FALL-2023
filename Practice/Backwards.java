e4d82eb9ad12b31f7ad87f54c8b647a0a57545b8
Michaelrbacu <michaelrbacu2@gmail.com>
2023-12-07 11:03:41 -0500

    Backwards.java program for practice

---
+++ Practice/Backwards.java
@@ -0,0 +1,41 @@
+// ******************************************************************
+//   Backwards.java
+//
+//   Uses a recursive method to print a string backwards.
+// ******************************************************************
+import java.util.Scanner;
+
+public class Backwards
+{
+
+    //--------------------------------------------------------------
+    // Reads a string from the user and prints it backwards.
+    //--------------------------------------------------------------
+    public static void main(String[] args)
+    {
+	String msg;
+	Scanner scan  = new Scanner (System.in);
+
+	System.out.print("Enter a string: ");
+	msg = scan.nextLine();
+	System.out.print("\nThe string backwards: ");
+	printBackwards(msg);
+	System.out.println();
+
+    }
+	
+	
+    //--------------------------------------------------------------
+    // Takes a string and recursively prints it backwards.
+    //--------------------------------------------------------------
+    public static void printBackwards(String s)
+    {
+	if (s.length() != 0)
+	    {
+		int lastIndex = s.length()-1;
+		System.out.print(s.charAt(lastIndex));
+		printBackwards(s.substring(0,lastIndex));
+	    }
+    }
+
+}
