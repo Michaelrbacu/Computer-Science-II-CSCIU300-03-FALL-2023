87fbc7e8711d7c51d75e8bd6b91f541eeba4d46f
michael <michaelrbacu2@gmail.com>
2023-12-12 22:44:59 -0500

    practicing

--- Assignments/10/LinkedQueue.java
+++ Assignments/10/LinkedQueue.java
@@ -1,73 +1,77 @@
-//***********************************************************
-// LinkedQueue.java
-// A linked-list implementation of the classic FIFO queue interface.
-//***********************************************************
+// *******************************************************************
+//   LinkedQueue.java
+//
+//   A linked-list implementation of the classic FIFO queue interface. 
+// *******************************************************************
+
 public class LinkedQueue implements QueueADT {
     private Node front, back;
     private int numElements;
 
-    // ---------------------------------------------
-    // Constructor; initializes the front and back pointers
+    // -------------------------------------------------------
+    // Constructor -- initializes the front and back pointers
     // and the number of elements.
-    // ---------------------------------------------
+    // -------------------------------------------------------
     public LinkedQueue() {
-        front = back = null;
+        front = null;
+        back = null;
         numElements = 0;
     }
 
-    // ---------------------------------------------
-    // Puts item on the end of the queue.
-    // ---------------------------------------------
+    // --------------------------------------------------
+    // Puts an item on the end of the queue
+    // --------------------------------------------------
     public void enqueue(Object item) {
         Node newNode = new Node(item);
-        if (isEmpty()) {
-            front = newNode;
-        } else {
-            back.setNext(newNode);
-        }
-        back = newNode;
         numElements++;
+            if(back == null){
+                front = newNode;
+            }else{
+                back.setNext(newNode);
+            }
+            back = newNode;
     }
 
-    // ---------------------------------------------
+    // -------------------------------------------------------
     // Removes and returns object from the front of the queue.
-    // ---------------------------------------------
+    // -------------------------------------------------------
     public Object dequeue() {
-        if (isEmpty()) {
-            System.out.println("Queue is empty. Cannot dequeue.");
-            return null;
-        }
-        Object item = front.getElement();
-        front = front.getNext();
-        numElements--;
-        if (isEmpty()) {
-            back = null;
+        Object item = null;
+        if (!isEmpty()) {
+            item = front.getElement();
+            front = front.getNext();
+            if (front == null)
+                back = null;
+            numElements--;
         }
         return item;
     }
 
-    // ---------------------------------------------
-    // Returns true if the queue is empty.
-    // ---------------------------------------------
+    // -----------------------------------------------------
+    // Returns true if the queue is empty, false otherwise.
+    // -----------------------------------------------------
     public boolean isEmpty() {
-        return front == null;
+        return numElements == 0;
     }
 
+    // ----------------------------------------------------
+    // Returns true if queue is full, but it never is.
+    // ----------------------------------------------------
     public boolean isFull() {
         return false;
     }
 
-    // ---------------------------------------------
+    // ------------------------------------------------------
     // Returns the number of elements in the queue.
-    // ---------------------------------------------
+    // ------------------------------------------------------
     public int size() {
         return numElements;
     }
 
-    // ---------------------------------------------
+    // --------------------------------------------------------
     // Returns a string containing the elements of the queue
-    // from first to last
-    // ---------------------------------------------
+    // from first to last.
+    // --------------------------------------------------------
     public String toString() {
         String result = "\n";
         Node temp = front;
@@ -75,6 +79,8 @@ public class LinkedQueue implements QueueADT {
             result += temp.getElement() + "\n";
             temp = temp.getNext();
         }
+
         return result;
     }
+
 }
--- Assignments/10/Matching/ParenMatch.java
+++ Assignments/10/Matching/ParenMatch.java
@@ -1,42 +1,43 @@
 package Matching;
 
+//**************************************************************
+// ParenMatch.java
+//
+// Determines whether or not a string of characters contains
+// matching left and right parentheses.
+//**************************************************************
+
 import java.util.*;
+import java.util.Scanner;
 
 public class ParenMatch {
     public static void main(String[] args) {
-        Stack<Character> s = new Stack<>();
-        String line;
+        Stack s = new Stack();
+        String line; // the string of characters to be checked
         Scanner scan = new Scanner(System.in);
+
         System.out.println("\nParenthesis Matching");
         System.out.print("Enter a parenthesized expression: ");
         line = scan.nextLine();
 
-        for (int i = 0; i < line.length(); i++) {
-            char currentChar = line.charAt(i);
-
-            if (currentChar == '(' || currentChar == ')') {
-                if (currentChar == '(') {
-
-                    s.push(currentChar);
-                } else {
-
-                    if (s.isEmpty()) {
-                        System.out.println("Error: Extra right parenthesis at position " + (i + 1));
-                        System.out.println(line.substring(0, i + 1));
-                        return;
-                    } else {
-
-                        s.pop();
-                    }
-                }
-            }
+        boolean ok = true;
+        for (int i = 0; i < line.length() & ok; i++) {
+            String ch = line.substring(i, i + 1);
+            if (ch.equals("("))
+                s.push(ch);
+            else if (ch.equals(")"))
+                if (s.empty()) {
+                    System.out.println("Extra right parenthesis");
+                    System.out.println(line.substring(0, i + 1));
+                    ok = false;
+                } else
+                    s.pop();
         }
 
-        if (s.isEmpty()) {
-            System.out.println("Parentheses match!");
-        } else {
-            System.out.println("Error: Extra left parenthesis");
-            System.out.println(line.substring(0, line.indexOf(s.peek())));
-        }
+        if (ok)
+            if (s.empty())
+                System.out.println("Parentheses Match");
+            else
+                System.out.println("More left parentheses than right");
     }
 }
